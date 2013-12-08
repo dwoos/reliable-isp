cbbd723bf741d52648fb774692da9663a09ece34
Doug Woos <doug@gamechanger.io>
2013-12-07 20:04:25 -0800

    script to start circuitd

---
+++ scripts/start_circuitd.sh
@@ -0,0 +1,9 @@
+case $(hostname) in
+    *.washington.vicci.org) CONFIG=isp1.json ;;
+    *.stanford.vicci.org ) CONFIG=isp2.json ;;
+    *.princeton.vicci.org) CONFIG=isp3.json ;;
+    *) exit ;;
+esac
+
+cd /reliable-isp
+nohup python daemons/circuitd.py $CONFIG &

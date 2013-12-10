a4f9ddb1cef453a423eda07f45939bb3428a09ec
Doug Woos <doug@gamechanger.io>
2013-12-09 23:53:53 -0800

    fix

--- scripts/create_circuits.sh
+++ scripts/create_circuits.sh
@@ -1,4 +1,4 @@
-for i in {1001..`expr $1+1000`}
+for i in $(seq 1001 `expr $1 + 1000`)
 do
     python /reliable-isp/clients/circuitc.py 10.128.114.14 10.128.114.22 10.128.114.26 10.128.114.31 $i 2> /dev/null|grep "0, taas=" | awk '{print $6}' | cut -c 6-
 done

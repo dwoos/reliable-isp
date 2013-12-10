a673acc68dea971cb5831d69ecf5fdd84ff60073
Doug Woos <doug@gamechanger.io>
2013-12-09 21:37:27 -0800

    client script

--- scripts/start_clients.sh
+++ scripts/start_clients.sh
@@ -1,4 +1,4 @@
-for i in {1001..1500}
+for i in {1001..1100}
 do
     echo "creating client for $i"
     service_id=$(python /reliable-isp/clients/circuitc.py 10.128.114.14 10.128.114.22 10.128.114.26 10.128.114.31 $i 2> /dev/null|grep "0, taas=" | awk '{print $6}' | cut -c 6-)

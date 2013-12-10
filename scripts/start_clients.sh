ee17c25e37dc9907398238faa226d7c3e1f91d5e
Doug Woos <doug@gamechanger.io>
2013-12-09 22:36:28 -0800

    update scripts

--- scripts/start_clients.sh
+++ scripts/start_clients.sh
@@ -3,6 +3,6 @@ for circuit in `cat $1`
 do
     n=`expr $i + 1000`
     echo "creating client for $n"
-    /reliable-isp/clients/udp_taas_client 10.128.114.11 `expr $n + 1000` $circuit 10.128.114.15 5 &> /tmp/clients/$n.log &
+    /reliable-isp/clients/udp_taas_client 10.128.114.11 `expr $n + 1000` $circuit 10.128.114.15 5 > /tmp/clients/$n.stdout 2> /tmp/clients/$n.stderr &
     i=`expr $i + 1`
 done

f76b4e08b34621a25341b946724dea428c649df7
Qiao Zhang <qiao@cs.washington.edu>
2014-01-28 13:03:46 -0800

    changed isp1 isp_node config

--- scripts/start_circuitd.sh
+++ scripts/start_circuitd.sh
@@ -1,5 +1,5 @@
 case $(hostname) in
-    node14.washington.vicci.org) CONFIG=ISP1_ING.json ;;
+    node14.washington.vicci.org) CONFIG=ISP1_EG.json ;;
     node26.washington.vicci.org ) CONFIG=ISP2_ING.json ;;
     node12.stanford.vicci.org ) CONFIG=ISP2_EG.json ;;
     node19.stanford.vicci.org ) CONFIG=ISP3_ING.json ;;

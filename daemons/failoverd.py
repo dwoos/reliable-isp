a82bd529d533e1741d099206861513c932e4fb74
Qiao Zhang <qiao@cs.washington.edu>
2014-01-30 12:49:44 -0800

    major revamp of failover daemon; simplified message rounds and use UDP to allow timeout for receive

--- daemons/failoverd.py
+++ daemons/failoverd.py
@@ -8,154 +8,115 @@ from kazoo.client import KazooClient
 import messages_pb2 as pb
 import subprocess
 
-ACK_TIMEOUT = 2
+CHECK_TIMEOUT = 2
 COMPLETE_TIMEOUT = 5
 
 PORT = 3457
+
+NON_RECURSIVE_CHECK = '0'
+RECURSIVE_CHECK = '1'
+
+PING_FAILURE = '0'
+PING_SUCCESS = '1'
+
+CHECK_FAILURE = '0'
+CHECK_SUCCESS = '1'
 
 config = json.loads(open(sys.argv[1]).read())
 
 zookeeper = KazooClient(hosts=','.join(config['zookeeper']))
 zookeeper.start()
 
-
 def get_my_ip():
-    #return subprocess.check_output(["ifconfig | grep 10.128 | awk '{print $2}' | cut -c 6-"],
-    #                               shell=True).strip()
     return subprocess.check_output(['curl', '-s', 'http://ipecho.net/plain'])
 
 class FailoverHandler(SocketServer.BaseRequestHandler):
-    def fail(self, req):
-        fail = pb.FailoverComplete()
-        fail.request.CopyFrom(req)
-        fail.success = False
-        self.request.send(fail.SerializeToString())
-        self.request.close()
-
-    def ack(self, req):
-        ack = pb.CheckFailoverAcknowledge()
-        ack.request.CopyFrom(req)
-        self.request.send(ack.SerializeToString())
-
-
-    def succeed(self, req):
-        success = pb.FailoverComplete()
-        success.request.CopyFrom(req)
-        success.success = True
-        self.request.send(success.SerializeToString())
-        self.request.close()
-
     def handle(self):
-        # self.request is the client connection
         print "Received failover check request"
         sys.stdout.flush()
-        data = self.request.recv(1024)  # clip input at 1Kb
-        req = pb.CheckFailover()
-        req.ParseFromString(data)
-        print "ACKing"
-        sys.stdout.flush()
-        self.ack(req)
 
-        if not req.should_forward:
-            print "no need to forward, so we're done"
-            sys.stdout.flush()
-            # cool, we're done
-            self.request.close()
+        # determine check message type
+        data = self.request[0].strip('\0')
+        check_type = data[0]
+        client_socket = self.request[1]
+
+        # if it's a standby ISP node; acknowledge ping
+        if check_type == NON_RECURSIVE_CHECK:
+            client_socket.sendto(PING_SUCCESS, self.client_address)
             return
+
         # now check for failure at the next hop
-        next_authenticator = int(zookeeper.get('/circuit/{0}/next_auth'.format(req.authenticator))[0])
-        if not next_authenticator:
+        authenticator = int(data[1:])
+        print authenticator
+        next_authenticator = int(zookeeper.get('/circuit/{0}/next_auth'.format(authenticator))[0])
+        print next_authenticator
+
+        # return CHECK_SUCCESS if we are at last hop ISP
+
+        # we arbitrarily decide that service id < 10000 in our experiments
+        # this is a hack, need better way to detect that we are at the last hop ISP
+        if next_authenticator < 10000:
             print "we're the last hop, so we can't check anymore. succeeding"
             sys.stdout.flush()
-            return self.succeed(req)
-        next_ip = zookeeper.get('/circuit/{0}/next_ip'.format(req.authenticator))[0]
+            client_socket.sendto(CHECK_SUCCESS, self.client_address)
+            return
 
-        print "going to check " + next_ip
+        # otherwise ping all next_ips
+        next_ip = zookeeper.get('/circuit/{0}/next_ip'.format(authenticator))[0]
+        next_ips = zookeeper.get('/circuit/{0}/next_ips'.format(authenticator))[0].split(',')
+
+        print "parallel ping " + str(next_ips)
         sys.stdout.flush()
-        next_req = pb.CheckFailover()
-        next_req.authenticator = next_authenticator
-        next_req.should_forward = True
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.settimeout(ACK_TIMEOUT)
-        try:
-            sock.connect((next_ip, PORT))
-            sock.send(next_req.SerializeToString())
-            data = sock.recv(1024)
-            ack = pb.CheckFailoverAcknowledge()
-            ack.ParseFromString(data)
-            assert ack.request == next_req
-        except socket.error:
-            print "ping of {0} timed out--we've found the problem!".format(next_ip)
-            sys.stdout.flush()
-            # we've found the failure
-            ping_req = pb.CheckFailover()
-            ping_req.authenticator = next_authenticator
-            ping_req.should_forward = False
-            ping_req_data = ping_req.SerializeToString()
-            next_ips = zookeeper.get('/circuit/{0}/next_ips'.format(req.authenticator))[0]
-            for ip in next_ips.split(','):
-                if ip == next_ip:
-                    continue
-                print "checking " + ip
-                sys.stdout.flush()
-                ping_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                ping_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                ping_sock.settimeout(ACK_TIMEOUT)
-                try:
-                    ping_sock.connect((ip, PORT))
-                    ping_sock.send(ping_req_data)
-                    data = ping_sock.recv(1024)
-                    ping_sock.close()
-                    ping_ack = pb.CheckFailoverAcknowledge()
-                    ping_ack.ParseFromString(data)
-                    assert ping_ack.request == ping_req
-                    # ok, this is our new ip
-                    print ip + " seems to work! let's use it."
-                    sys.stdout.flush()
-                    zookeeper.set('/circuit/{0}/next_ip'.format(req.authenticator), ip)
-                    return self.succeed(req)
-                except Exception as e:
-                    print e
-                    sys.stdout.flush()
-                    print ip + " failed"
-                    sys.stdout.flush()
-                    continue
-            # ok, we're out of ips. fail!
-            return self.fail(req)
-        except e:
-            # some other error, bail
-            print e
-            sys.stdout.flush()
-            return self.fail(req)
-        else:
-            # wait for complete, then return to client
+        # initialize ping results to be all failures = 0
+        ping_results = [CHECK_FAILURE] * len(next_ips)
+
+        for i in xrange(len(next_ips)):
+            ip = next_ips[i]
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.settimeout(CHECK_TIMEOUT)
+
+            print ip, PORT
             try:
-                sock.settimeout(COMPLETE_TIMEOUT)
-                data = sock.recv(1024)
-                sock.close()
-                complete = pb.FailoverComplete()
-                complete.ParseFromString(data)
-                assert complete.request == next_req
-                if complete.success:
-                    return self.succeed(req)
-                return self.fail(req)
-            except:
-                return self.fail(req)
+                if ip == next_ip:
+                    # send recursive check
+                    recursive_check_msg = RECURSIVE_CHECK + str(next_authenticator)
+                    sock.sendto(recursive_check_msg, (ip, PORT))
+                    received = sock.recv(1024)
+                    print "receive from recursive check = " + received
+                else:
+                    # send recursive check
+                    non_recursive_check_msg = NON_RECURSIVE_CHECK
+                    sock.sendto(non_recursive_check_msg, (ip, PORT))
+                    received = sock.recv(1024)
+                    print "receive from non_recursive check = " + received
+                # update ping results
+                ping_results[i] = received
+            except socket.timeout:
+                print "timed out on pinging " + ip
+                ping_results[i] = CHECK_FAILURE
 
+        print "parallel ping results = " + str(ping_results)
 
-class SimpleServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+        client_socket.sendto(CHECK_FAILURE, self.client_address)
+
+        #print ip + " seems to work! let's use it."
+        #sys.stdout.flush()
+        #zookeeper.set('/circuit/{0}/next_ip'.format(req.authenticator), ip)
+        #return self.succeed(req)
+
+class SimpleServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
     # Ctrl-C will cleanly kill all spawned threads
     daemon_threads = True
     # much faster rebinding
     allow_reuse_address = True
 
     def __init__(self, server_address, RequestHandlerClass):
-        SocketServer.TCPServer.__init__(self, server_address, RequestHandlerClass)
+        SocketServer.UDPServer.__init__(self, server_address, RequestHandlerClass)
 
 if __name__ == "__main__":
-    server = SimpleServer((get_my_ip(), 3457), FailoverHandler)
+    server = SimpleServer((get_my_ip(), PORT), FailoverHandler)
     # terminate with Ctrl-C
     try:
         print "gonna serve"
